734d31fa4f737c5f5f335be711bd072e666aeda1
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2026-01-08 17:04:06 +0200

    Next

---
+++ alx-rick-and-morty-app/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
